faac791012f2fcced0dee6937bef0f7ef361b497
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-06 17:38:08 +0100

    add Stats component

---
+++ src/components/Stats/Stats.tsx
@@ -0,0 +1,16 @@
+import { StyledListContainer,StyledListBaner, StyledUl,StyledListItem } from '../../styles/StyledList';
+
+
+
+
+export const Stats= ()=>{
+
+    return <StyledListContainer>
+        <StyledListBaner>Statistic</StyledListBaner>
+        <StyledUl>
+         <StyledListItem>Marcin Lopian</StyledListItem>
+         <StyledListItem>Marcin Lopian</StyledListItem>
+         <StyledListItem>Marcin Lopian</StyledListItem>
+        </StyledUl>
+    </StyledListContainer>
+}
