6561f406b8650fdf34d7689803f79a5ca4246580
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-06 16:53:13 +0100

    add Background to app

--- src/components/FootballApp/styleFootballApp.tsx
+++ src/components/FootballApp/styleFootballApp.tsx
@@ -5,4 +5,8 @@ export const StyledMainContainer = styled.div`
     flex-direction:column;
     width: 100%;
     min-height: 100vh;
+    align-items: center;
+    background: #a8c0ff;
+    background: -webkit-linear-gradient(to right, #a8c0ff, #3f2b96); 
+    background: linear-gradient(to right, #a8c0ff, #3f2b96);
 `
