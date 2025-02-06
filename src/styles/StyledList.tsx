6b78c89762dc959bc1e9bcda732ff207d268b439
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-06 17:31:56 +0100

    add styles of List

---
+++ src/styles/StyledList.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const StyledListBaner = styled.h2`
+    
+    font-size: 1.9rem;
+    padding: 0.5rem;
+    
+
+`
+export const StyledListContainer = styled.div`
+    
+    display: flex;
+    flex-direction: column;
+    padding-top: 0.5rem;
+`
+
+export const StyledUl = styled.ul`
+    
+    border: 3px solid black;
+
+`
+
+export const StyledListItem = styled.li`
+    
+    font-size: 1.3rem;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+`
