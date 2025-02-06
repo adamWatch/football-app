e38c16ef683b1bd3b1d9ec9afa4bddec037be24c
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-06 17:38:26 +0100

    add TeamsBase component

---
+++ src/components/TeamsBase/TeamsBase.tsx
@@ -0,0 +1,16 @@
+import { StyledListContainer,StyledListBaner, StyledUl,StyledListItem } from '../../styles/StyledList';
+
+
+
+
+export const TeamsBase = ()=>{
+
+    return <StyledListContainer>
+        <StyledListBaner>Teams List</StyledListBaner>
+        <StyledUl>
+         <StyledListItem>Marcin Lopian</StyledListItem>
+         <StyledListItem>Marcin Lopian</StyledListItem>
+         <StyledListItem>Marcin Lopian</StyledListItem>
+        </StyledUl>
+    </StyledListContainer>
+}
