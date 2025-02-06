54f3c12f554deac109ec9bcdf2c774081b923eb0
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-06 17:31:26 +0100

    Create Player Base component

---
+++ src/components/PlayerBase/PlayerBase.tsx
@@ -0,0 +1,16 @@
+import { StyledListContainer,StyledListBaner, StyledUl,StyledListItem } from '../../styles/StyledList';
+
+
+
+
+export const PlayerBase = ()=>{
+
+    return <StyledListContainer>
+        <StyledListBaner>Player List</StyledListBaner>
+        <StyledUl>
+         <StyledListItem>Marcin Lopian</StyledListItem>
+         <StyledListItem>Marcin Lopian</StyledListItem>
+         <StyledListItem>Marcin Lopian</StyledListItem>
+        </StyledUl>
+    </StyledListContainer>
+}
