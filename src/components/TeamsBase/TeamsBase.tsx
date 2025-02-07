225e8b9aa1661da78b7e3144103b7726d26ccf53
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-07 16:48:25 +0100

    add table and fetch

--- src/components/TeamsBase/TeamsBase.tsx
+++ src/components/TeamsBase/TeamsBase.tsx
@@ -1,16 +1,61 @@
-import { StyledListContainer,StyledListBaner, StyledUl,StyledListItem } from '../../styles/StyledList';
+import { useQuery } from '@tanstack/react-query';
+import { StyledListContainer,StyledListBaner,} from '../../styles/StyledList';
+
+import { StyledTable,  StyledTd, StyledTh,  } from '../../styles/StyledTable';
+
+interface Team {
+    id: number;
+    teamName: string;
+    foundingYear: number;
+    location: string;
+}
+
+
+const FetchTeamList = () =>{
+
+    const {data, isLoading} = useQuery({
+        queryKey: ['teams'],
+        queryFn: async () => {
+            const response = await fetch('http://localhost:3000/teams');
+            return response.json();
+        }
+        
+    })
+    const teamsData = {
+        currId: data ? data[0].currId: null,
+        teamsList: data ? data.slice(1):[],
+        isLoading
+    }
+    return teamsData;
+}
 
 
 
 
 export const TeamsBase = ()=>{
+    const teamsData = FetchTeamList();
+
+
 
     return <StyledListContainer>
-        <StyledListBaner>Teams List</StyledListBaner>
-        <StyledUl>
-         <StyledListItem>Marcin Lopian</StyledListItem>
-         <StyledListItem>Marcin Lopian</StyledListItem>
-         <StyledListItem>Marcin Lopian</StyledListItem>
-        </StyledUl>
+        <StyledListBaner>Team List</StyledListBaner>
+        <StyledTable>
+                <thead>
+                    <tr>
+                        <StyledTh>Name</StyledTh>
+                        <StyledTh>Founding Year</StyledTh>
+                        <StyledTh>Location</StyledTh>
+                    </tr>
+                </thead>
+                <tbody>
+                    {!teamsData.isLoading && teamsData.teamsList.map((team:Team) => (
+                        <tr key={team.id}>
+                            <StyledTd>{team.teamName}</StyledTd>
+                            <StyledTd>{team.foundingYear}</StyledTd>
+                            <StyledTd>{team.location}</StyledTd>
+                        </tr>
+                    ))}
+                </tbody>
+            </StyledTable>
     </StyledListContainer>
 }
