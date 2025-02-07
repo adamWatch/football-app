97012a2a746087be6b7fdfab49d4fd2e8b0d358a
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-07 14:19:24 +0100

    implement chage tab function

--- src/components/FootballApp/FootballApp.tsx
+++ src/components/FootballApp/FootballApp.tsx
@@ -1,15 +1,38 @@
+import { useState } from 'react'
 import { Baner } from '../Baner/Baner'
 import { Menu } from '../Menu/Menu'
+import { PlayerBase} from '../PlayerBase/PlayerBase'
 import {  StyledMainContainer } from './styleFootballApp'
+import { TeamsBase } from '../TeamsBase/TeamsBase'
+import { MatchBase } from '../MatchBase/MatchBase'
+import { Stats } from '../Stats/Stats'
+
 
 
 export const FootballApp = () =>{
 
+    const [currTab,setCurrTab] = useState('Player Base');
+
+    const renderPage = () => {
+        switch(currTab){
+            case 'Player Base':
+                return <PlayerBase/>
+            case 'Teams Base':
+                return <TeamsBase/>
+            case 'Match Base':
+                return <MatchBase/>
+            case 'Stats':
+                return <Stats/>
+            default:
+                return <PlayerBase/>
+        }
+    }
 
     return(
         <StyledMainContainer>
-            <Baner/>
-            <Menu/>
+            <Baner/> 
+            <Menu changeTab = {setCurrTab} />
+            {renderPage()}
         </StyledMainContainer>
     )
 }
