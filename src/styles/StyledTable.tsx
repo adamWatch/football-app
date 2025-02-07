dc2cd0884b1e15631832408b8a23755f7f4f640a
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-07 15:47:53 +0100

    add table styles

---
+++ src/styles/StyledTable.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const StyledTable = styled.table`
+ 
+ border: 3px solid black;
+ width: 100%; 
+ padding: 1rem;
+
+`
+
+
+export const StyledTh = styled.th`
+    border: 3px solid black;
+    font-size: 1.8rem;
+    padding: 0.5rem;
+`
+
+export const StyledTd = styled.td`
+    border: 3px solid black;
+    font-size: 1.5rem;
+    padding: 0.5rem;
+`
