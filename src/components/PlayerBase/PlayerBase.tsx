1c1177cecad63c5f78de34c981a7822a6e163547
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-21 17:09:56 +0100

    show notice and change text of notice

--- src/components/PlayerBase/PlayerBase.tsx
+++ src/components/PlayerBase/PlayerBase.tsx
@@ -1,21 +1,30 @@
 
-import { useState } from 'react';
+import { useEffect, useState} from 'react';
 import { StyledListContainer,StyledListBaner, } from '../../styles/StyledList';
 import { StyledTable,  StyledTd, StyledTh,  } from '../../styles/StyledTable';
 import { Player } from '../../types/Player';
 import { FetchList } from '../../utils/FetchList';
 import { AddToList } from '../AddToList/AddToList';
 import { StyledOverlay } from '../../styles/StyledForm';
+import { Notice } from '../Notice/Notice';
 
 
 export const PlayerBase = ()=>{
     const [addForm, setAddForm] = useState(false);
+    const [isNotice,setIsNotice] = useState(false);
     const playersData = FetchList('players');
+    const [notice,setNotice] = useState('');
+  
 
    const showAddForm = () => {
         setAddForm(!addForm);
     }
 
+    const showNotice = () => {
+        setIsNotice(!isNotice);
+    }
+    
+    console.log(playersData)
 
     return <StyledListContainer>
         <StyledListBaner>Players List</StyledListBaner>
@@ -34,11 +43,12 @@ export const PlayerBase = ()=>{
                             <StyledTd>{player.playerSurname}</StyledTd>
                             <StyledTd>{player.playerTeam}</StyledTd>
                         </tr>
-                    ))}
+                    ))} 
                 </tbody>
             </StyledTable>
-        {addForm &&<AddToList showAddForm ={showAddForm}/> }
-        {addForm && <StyledOverlay/>}
+        {addForm &&<AddToList showAddForm ={showAddForm} showNotice = {showNotice} setNotice = {setNotice} />  } 
+        {addForm &&<StyledOverlay/>}
+        {isNotice && <Notice text={notice}/>}
         <button onClick={showAddForm}>Add</button>    
         <button>Edit</button>    
     </StyledListContainer>
