e11984b75f8ced44bf8d884a25580bc673036216
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-21 17:09:31 +0100

    add style to notice

---
+++ src/components/Notice/styleNotice.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledNoticed = styled.div`
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5rem;
+    position: absolute;
+    top: 20%;
+    right: 20%;
+`
