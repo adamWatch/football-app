587c97d1616b6d614b779014041e261db47a0d5a
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-26 16:09:09 +0100

    add DataTypes

---
+++ src/types/DataTypes.ts
@@ -0,0 +1,9 @@
+import { Player } from './Player';
+import { Match } from './Match';
+import { Team } from './Team';
+
+export type DataTypes = {
+    players: Player;
+    matches: Match;
+    teams: Team;
+};
