9cf7d6c9b71ee07d9e2fcf9920ca8019fcca6996
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-27 14:56:46 +0100

    add Style to EditList

--- src/styles/StyledList.tsx
+++ src/styles/StyledList.tsx
@@ -13,3 +13,16 @@ export const StyledListContainer = styled.div`
     flex-direction: column;
     padding-top: 0.5rem;
 `
+
+export const StyledEditListContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    background-color: aliceblue;
+    align-items: center;
+    justify-content: center;
+    padding: 1rem;
+    top: 20%;
+    left: 20%;
+    z-index: 19;
+`
