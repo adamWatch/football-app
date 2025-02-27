675f591baeb850221997c2221aea2b489f44e9a2
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-27 19:58:48 +0100

    implement Edit and Delete

--- src/components/EditList/EditPlayerList.tsx
+++ src/components/EditList/EditPlayerList.tsx
@@ -1,21 +1,51 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { StyledFormBaner, StyledFormBtnBack } from '../../styles/StyledForm';
+import { StyledFormBaner, StyledFormBtnBack, StyledOverlay } from '../../styles/StyledForm';
 import { StyledEditListContainer} from '../../styles/StyledList';
 import { StyledTable, StyledTd, StyledTh } from '../../styles/StyledTable';
 import { Player } from '../../types/Player';
 import { FetchList } from '../hooks/FetchList';
 import { faX } from '@fortawesome/free-solid-svg-icons/faX';
+import { useDeleteItem } from '../hooks/useDeleteItem';
+import { useState } from 'react';
+import { EditPlayerForm } from '../EditForm/EditPlayerForm';
 
 
 interface EditListProps {
-    type:string
     showEditMode:()=>void;
 }
 
 export const EditPlayerList = (props:EditListProps) => {
 
-        const {type,showEditMode} = props;
-    
+        const {showEditMode} = props;
+
+        const deleteItem = useDeleteItem('players');
+        const [isEdit, setIsEdit] = useState(false);
+        const [editPlayerData, setEditPlayerData] = useState<Player>({
+            id:'',
+            playerName:'',
+            playerSurname:'',
+            playerTeam:'none'
+        });
+
+        const deletePlayer = (id:string) => {
+            if (window.confirm('Are you sure you want to delete this player?')) {
+                deleteItem(id)
+        };
+        }
+        
+        const showEditForm = () => {
+            setIsEdit(!isEdit); 
+        }
+
+        const handleEditPlayer = (player:Player) => {
+            setEditPlayerData(player);
+            showEditForm();
+            
+        }
+
+
+
+       
         const playersData = FetchList('players');
     
         return(
@@ -39,12 +69,14 @@ export const EditPlayerList = (props:EditListProps) => {
                                 <StyledTd>{player.playerName}</StyledTd>
                                 <StyledTd>{player.playerSurname}</StyledTd>
                                 <StyledTd>{player.playerTeam}</StyledTd>
-                                <StyledTd><button>Edit</button></StyledTd>
-                                {player.playerTeam === 'none' ? <StyledTd><button>Delete</button></StyledTd> : <StyledTd><button disabled>Delete</button></StyledTd>}
+                                <StyledTd><button onClick={()=>handleEditPlayer(player)}>Edit</button></StyledTd>
+                                {player.playerTeam === 'none' ? <StyledTd><button onClick={()=>deletePlayer(player.id)}>Delete</button></StyledTd> : <StyledTd><button disabled>Delete</button></StyledTd>}
                             </tr>
                         ))} 
                     </tbody>
                 </StyledTable>
+                {isEdit && <StyledOverlay/> }
+                {isEdit && <EditPlayerForm showEditForm={showEditForm} editPlayerData={editPlayerData} />}        
                 </StyledEditListContainer>
         )
 
