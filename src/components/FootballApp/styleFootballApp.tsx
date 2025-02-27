17de1c84fe10652c7f53343c4495d46cc98b1fb9
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-27 14:57:37 +0100

    change min-height

--- src/components/FootballApp/styleFootballApp.tsx
+++ src/components/FootballApp/styleFootballApp.tsx
@@ -4,7 +4,7 @@ export const StyledMainContainer = styled.div`
     display: flex;
     flex-direction:column;
     width: 100%;
-    min-height: 100vh;
+    min-height: 200vh;
     align-items: center;
     background: #a8c0ff;
     background: -webkit-linear-gradient(to right, #a8c0ff, #3f2b96); 
