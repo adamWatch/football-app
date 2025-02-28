74f9453c6003e6910307e549e329a2222c5c4139
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-28 11:56:54 +0100

    add Style to buttons

--- src/styles/StyledEditMode.tsx
+++ src/styles/StyledEditMode.tsx
@@ -39,3 +39,43 @@ export const StyledEditBtn = styled.button`
 
 
 `
+
+export const StyledEditModeBtn = styled.button`
+    position: fixed;
+    top: 15%;
+    right:10%;
+    background-color: #c4b142;
+    color: azure;
+    font-size: 1.5rem;
+    padding: 0.5rem;
+    margin: 0.5rem;
+    border: none;
+    border-radius: 5px;
+    transition: 0.3s linear;
+    cursor: pointer;
+    &:hover{
+        background-color: white;
+        color: #6bdfa7;
+    }
+
+
+`
+
+export const StyledAddBtn = styled.button`
+    position: fixed;
+    top: 15%;
+    right:5%;
+    background-color: #6bdfa7;
+    color: azure;
+    font-size: 1.5rem;
+    padding: 0.5rem;
+    margin: 0.5rem;
+    border: none;
+    border-radius: 5px;
+    transition: 0.3s linear;
+    cursor: pointer;
+    &:hover{
+        background-color: white;
+        color: #6bdfa7;
+    }
+`
