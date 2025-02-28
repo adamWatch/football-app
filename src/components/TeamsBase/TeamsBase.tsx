bdda89b0fc43672986443ab291acc3a9d8d0a892
Adam Watchmaker <watchmaker1337@gmail.com>
2025-02-28 12:25:16 +0100

    update TeamsBase component

--- src/components/TeamsBase/TeamsBase.tsx
+++ src/components/TeamsBase/TeamsBase.tsx
@@ -1,19 +1,38 @@
-import { useQuery } from '@tanstack/react-query';
-import { StyledListContainer,StyledListBaner,} from '../../styles/StyledList';
-
-import { StyledTable,  StyledTd, StyledTh,  } from '../../styles/StyledTable';
+import { useState } from 'react';
+import { StyledListContainer, StyledListBaner } from '../../styles/StyledList';
+import { StyledTable, StyledTd, StyledTh } from '../../styles/StyledTable';
 import { Team } from '../../types/Team';
 import { FetchList } from '../hooks/FetchList';
+import { AddTeamToList } from '../AddToList/AddTeamToList';
+import { StyledOverlay } from '../../styles/StyledForm';
+import { Notice } from '../Notice/Notice';
+import { EditTeamList } from '../EditList/EditTeamList';
+import { StyledAddBtn, StyledEditModeBtn } from '../../styles/StyledEditMode';
 
-
-export const TeamsBase = ()=>{
+export const TeamsBase = () => {
+    const [addForm, setAddForm] = useState(false);
+    const [editMode, setEditMode] = useState(false);
+    const [isNotice, setIsNotice] = useState(false);
     const teamsData = FetchList('teams');
+    const [notice, setNotice] = useState('');
 
+    const showAddForm = () => {
+        setAddForm(!addForm);
+    };
 
+    const showEditMode = () => {
+        setEditMode(!editMode); 
+    }
 
-    return <StyledListContainer>
-        <StyledListBaner>Teams List</StyledListBaner>
-        <StyledTable>
+    const showNotice = () => {
+        setIsNotice(!isNotice);
+        setTimeout(() => setIsNotice(false), 2500);
+    };
+
+    return (
+        <StyledListContainer>
+            <StyledListBaner>Teams List</StyledListBaner>
+            <StyledTable>
                 <thead>
                     <tr>
                         <StyledTh>Name</StyledTh>
@@ -22,7 +41,7 @@ export const TeamsBase = ()=>{
                     </tr>
                 </thead>
                 <tbody>
-                    {!teamsData.isLoading && teamsData.list.map((team:Team) => (
+                    {!teamsData.isLoading && teamsData.list.map((team: Team) => (
                         <tr key={team.id}>
                             <StyledTd>{team.teamName}</StyledTd>
                             <StyledTd>{team.foundingYear}</StyledTd>
@@ -31,5 +50,19 @@ export const TeamsBase = ()=>{
                     ))}
                 </tbody>
             </StyledTable>
-    </StyledListContainer>
-}
+            {addForm && (
+                <AddTeamToList
+                    showAddForm={showAddForm}
+                    showNotice={showNotice}
+                    setNotice={setNotice}
+                />
+            )}
+            {editMode &&<EditTeamList showEditMode={showEditMode}/> }
+            {addForm && <StyledOverlay />}
+            {editMode &&<StyledOverlay/>}
+            {isNotice && <Notice text={notice} />}
+            <StyledAddBtn onClick={showAddForm}>Add</StyledAddBtn>    
+            <StyledEditModeBtn onClick={showEditMode}>Edit</StyledEditModeBtn>
+        </StyledListContainer>
+    );
+};
